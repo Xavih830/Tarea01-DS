dd05326974389839282a0c32d5d309ff1f92d136
Anlaye22 <119507384+Anlaye22@users.noreply.github.com>
2024-11-15 22:39:49 -0500

    Update Soporte.java

--- tareagrupal/src/main/java/com/disenosoftware/Soporte.java
+++ tareagrupal/src/main/java/com/disenosoftware/Soporte.java
@@ -4,4 +4,9 @@ public class Soporte {
     public void gestionarReporte(Pronostico pronostico){
         System.out.println("Reporte gestionado para el pronóstico: " + pronostico.prediccion);
     }
+
+    public void escalarReporte(Pronostico pronostico){
+        System.out.println("Reporte escalado a Control de Calidad para el pronóstico: " + pronostico.prediccion);
+        
+    }
 }
